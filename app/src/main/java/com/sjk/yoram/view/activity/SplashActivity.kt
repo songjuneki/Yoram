f8997d6d5778e2775666bdfcfa042157cd68e270
songjuenki <dev.sjk0105@gmail.com>
2023-10-18 16:36:52 +0900

    feat: 안드로이드 12 이상 호환 SplashScreen 구현

--- app/src/main/java/com/sjk/yoram/view/activity/SplashActivity.kt
+++ app/src/main/java/com/sjk/yoram/view/activity/SplashActivity.kt
@@ -2,10 +2,14 @@ package com.sjk.yoram.view.activity
 
 import android.content.DialogInterface
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.databinding.DataBindingUtil
 import com.sjk.yoram.model.LoginCheck
 import com.sjk.yoram.repository.retrofit.MyRetrofit
@@ -14,18 +18,30 @@ import com.sjk.yoram.databinding.SplashBinding
 import com.sjk.yoram.model.dto.LoginResult
 import com.sjk.yoram.view.activity.main.MainActivity
 import kotlinx.coroutines.*
-import kotlin.system.exitProcess
 
 class SplashActivity: AppCompatActivity() {
     private lateinit var binding: SplashBinding
+    private lateinit var splashScreen: SplashScreen
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        splashScreen = installSplashScreen()
         binding = DataBindingUtil.setContentView(this, R.layout.splash)
-        val serverCheck = CoroutineScope(Dispatchers.Default).async { MyRetrofit.checkServer() }
-        suspend fun isLogin(id:Int, pw:String) = CoroutineScope(Dispatchers.Default).async { MyRetrofit.userApi.check(LoginCheck(id, pw)) }
+
 
         CoroutineScope(Dispatchers.Main).launch {
+            if (!checkInternetConnection()) {
+                AlertDialog.Builder(this@SplashActivity)
+                    .setMessage("인터넷 연결이 없습니다. 인터넷 연결을 확인해 주세요")
+                    .setPositiveButton("확인") { _, _ -> ActivityCompat.finishAffinity(this@SplashActivity) }
+                    .create()
+                    .show()
+                delay(50000)
+            }
+
+            val serverCheck = CoroutineScope(Dispatchers.Default).async { MyRetrofit.checkServer() }
+            suspend fun isLogin(id:Int, pw:String) = CoroutineScope(Dispatchers.Default).async { MyRetrofit.userApi.check(LoginCheck(id, pw)) }
+
             if (serverCheck.await()) {
                 val initIntent = Intent(this@SplashActivity, InitActivity::class.java)
                 val homeIntent = Intent(this@SplashActivity, MainActivity::class.java)
@@ -54,12 +70,11 @@ class SplashActivity: AppCompatActivity() {
                         finish()
                     }
                 }
-            }
-            else {
+            } else {
                 val builder = AlertDialog.Builder(this@SplashActivity)
                 builder.setMessage("서버 연결에 실패하였습니다.\n 애플리케이션을 다시 실행해 주세요.\n 문제가 계속 되면 관리자에게 문의바랍니다.")
                     .setPositiveButton("확인"
-                    ) { _, _ -> ActivityCompat.finishAffinity(this@SplashActivity); exitProcess(0) }
+                    ) { _, _ -> ActivityCompat.finishAffinity(this@SplashActivity) }
                     .create().show()
             }
         }
@@ -78,6 +93,24 @@ class SplashActivity: AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         overridePendingTransition(0 , 0)
+    }
+
+
+    private suspend fun checkInternetConnection(): Boolean = withContext(Dispatchers.Main) {
+        try {
+            val conManager = application.getSystemService(ConnectivityManager::class.java)
+            val currentNetwork = conManager.activeNetwork ?: return@withContext false
+            val caps = conManager.getNetworkCapabilities(currentNetwork) ?: return@withContext false
+
+            return@withContext caps.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                    caps.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                    caps.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) ||
+                    caps.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) ||
+                    caps.hasTransport(NetworkCapabilities.TRANSPORT_VPN) ||
+                    caps.hasTransport(NetworkCapabilities.TRANSPORT_USB)
+        } catch (e: Exception) {
+            return@withContext false
+        }
     }
 
 }
